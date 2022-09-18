4d6b6586fa1541d226609e5421f7d3de1eeff16c
Evan <endyce@gmail.com>
2022-09-17 19:50:57 -0700

    Redesign homescreen

--- quickstart-java/app/src/main/java/com/hypertrack/quickstart/MainActivity.java
+++ quickstart-java/app/src/main/java/com/hypertrack/quickstart/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements TrackingStateObse
     private static final String TAG = "MainActivity";
     private static final String PUBLISHABLE_KEY = "_-_s4Di-y9a1-jEHY1flQT-RdG-Oun-8L0M88x5f8OIu8OMhgbQdjgEqcPOwGr6Kc5RRgsZxLQDnFYb0VtQxVQ";
 
-    private TextView trackingStatusLabel;
     private HyperTrack sdkInstance;
     private boolean shouldRequestPermissions = true;
 
@@ -35,8 +34,6 @@ public class MainActivity extends AppCompatActivity implements TrackingStateObse
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
 
-        TextView deviceId = findViewById(R.id.deviceIdTextView);
-        trackingStatusLabel = findViewById(R.id.statusLabel);
         HyperTrack.enableDebugLogging();
 
         sdkInstance = HyperTrack
@@ -44,31 +41,6 @@ public class MainActivity extends AppCompatActivity implements TrackingStateObse
                 .addTrackingListener(this);
         sdkInstance.start();
 
-        Button button = findViewById(R.id.button_query_position);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                HyperTrackViews hypertrackView = HyperTrackViews.getInstance(getApplicationContext(), Keys.PUBLISHABLE_KEY);
-
-                hypertrackView.getDeviceMovementStatus(sdkInstance.getDeviceID(),
-                        new Consumer<MovementStatus>() {
-                            @Override
-                            public void accept(MovementStatus movementStatus) {
-                                com.hypertrack.sdk.views.dao.Location location = movementStatus.location;
-                                if (location != null) {
-                                    Log.d(TAG, "Got device location " + location +
-                                            " with latitude " + location.getLatitude() +
-                                            " and longitude " + location.getLongitude());
-                                }
-                            }
-                        });
-            }
-        });
-
-
-
-
-        deviceId.setText(sdkInstance.getDeviceID());
         Log.d(TAG, "device id is " + sdkInstance.getDeviceID());
     }
 
@@ -99,36 +71,10 @@ public class MainActivity extends AppCompatActivity implements TrackingStateObse
     @Override
     public void onError(TrackingError trackingError) {
         Log.d(TAG, "onError: " + trackingError.message);
-        trackingStatusLabel.setBackgroundColor(ContextCompat.getColor(this, R.color.colorLabelError));
-        switch (trackingError.code) {
-            case TrackingError.INVALID_PUBLISHABLE_KEY_ERROR:
-            case TrackingError.AUTHORIZATION_ERROR:
-                trackingStatusLabel.setText(R.string.check_publishable_key);
-                break;
-            case TrackingError.GPS_PROVIDER_DISABLED_ERROR:
-                trackingStatusLabel.setText(R.string.enable_gps);
-                break;
-            case TrackingError.PERMISSION_DENIED_ERROR:
-                trackingStatusLabel.setText(R.string.permissions_not_granted);
-                break;
-            default:
-                trackingStatusLabel.setText(R.string.cant_start_tracking);
-
-        }
     }
 
     @Override
     public void onTrackingStart() {
-
-        trackingStatusLabel.setBackgroundColor(ContextCompat.getColor(this, R.color.colorLabelActive));
-        trackingStatusLabel.setText(R.string.tracking_active);
-        Location top_right = new Location("provider-name");
-        top_right.setLatitude(43.473410);
-        top_right.setLongitude(-80.541037);
-
-        Location bottom_left = new Location("provider-name");
-        bottom_left.setLatitude(43.473279);
-        bottom_left.setLongitude(-80.539559);
 
         new ComputingPosition(this, this.sdkInstance).start();
 
@@ -136,9 +82,6 @@ public class MainActivity extends AppCompatActivity implements TrackingStateObse
 
     @Override
     public void onTrackingStop() {
-
-        trackingStatusLabel.setBackgroundColor(ContextCompat.getColor(this, R.color.colorLabelStopped));
-        trackingStatusLabel.setText(R.string.tracking_stopped);
 
     }
 
